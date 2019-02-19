12f6e181cbaa9e68be1702d758923b36e4806ce2
root <root@localhost.localdomain>
2019-02-19 06:49:38 +0000

    2.19b

--- app/Http/Controllers/Weixin/WeixinController.php
+++ app/Http/Controllers/Weixin/WeixinController.php
@@ -78,7 +78,7 @@ class WeixinController extends Controller
     {
         // 文本消息
         $xml_response = '<xml><ToUserName><![CDATA['.$openid.']]></ToUserName><FromUserName><![CDATA['.$from.']]></FromUserName><CreateTime>'.time().'</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA['. 'Hello World, 现在时间'. date('Y-m-d H:i:s') .']]></Content></xml>';
-        return $xml_response;
+        echo $xml_response;
     }
 
 
