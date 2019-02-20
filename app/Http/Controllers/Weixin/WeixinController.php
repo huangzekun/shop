cf8f8a6e6a937950499de793a6406548e60c7223
root <root@localhost.localdomain>
2019-02-20 02:10:16 +0000

    2.20c

--- app/Http/Controllers/Weixin/WeixinController.php
+++ app/Http/Controllers/Weixin/WeixinController.php
@@ -87,7 +87,7 @@ class WeixinController extends Controller
             }
         }
         $log_str = date('Y-m-d H:i:s') . "\n" . $data . "\n<<<<<<<";
-        //file_put_contents('logs/wx_event.log',$log_str,FILE_APPEND);
+        file_put_contents('logs/wx_event.log',$log_str,FILE_APPEND);
     }
 
     /**
